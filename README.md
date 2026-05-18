c80e75fdacdb26a89358005f5df43487630a8e79
Sharifjon Sharipov <ss2393506@gmail.com>
2026-05-18 19:08:21 +0200

    stripped down the visuals

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
-pg.sh - static webpage generator for your files.
+# Static webpage generator for your files.
 
-usage:
-    copy "pg.sh", "style.pg-css", "templh", and "templt" into a certain directory (e.g. "mydir") and run:
-        $ ./pg.sh
+### Usage:
+copy `pg.sh` into your desired directory and execute it. It will generate .pg.html files; open them on your web browser.
 
-It will generate html files. Open "mydir.pg-html" on your web browser.
+### Cons:
+- can be slow for big directories.
 
-cons:
-    - can be slow for big directories.
+### Bugs:
+- doesn't work as expected if there are files/directories that have spaces in their names.
 
-bugs:
-    - doesn't work as expected if there are files/directories that have spaces in their names.
-
-To clean up generated files, run:
-        $ ./clean.sh
+### To clean up generated files, run:
+```
+$ ./clean.sh
+```
+### Note
+If you want a nicer look, and displaying of the file sizes, checkout this [commit](https://github.com/ssharifjon/pg/commit/a1254a2b15b6bbd56db278b781a95123ca6e1f10).
 
--- pg.sh
+++ pg.sh
@@ -4,46 +4,53 @@ dirs=$(find . -type d -printf "%P\n")
 files=$(find . -type f -printf "%P\n")
 
 curdir=$(basename $(pwd))
-cat templh > ${curdir}.pg-html
-printf "\t\t\t%s\n" "<tr><th>/</th></tr>" >> ${curdir}.pg-html
+printf '<!DOCTYPE html>
+<html>
+	<body>
+		<table>
+' > ${curdir}.pg.html
+printf "\t\t\t%s\n" "<tr><td><b>/</b></td></tr>" >> ${curdir}.pg.html
 items=$(ls --group-directories-first)
 for item in $items; do
 	if [[ -d $item ]]; then
-		printf "\t\t\t%s\n" "<tr><td class="dir"><a href="${item}/${item}.pg-html">$item</a></td><td class="size">-</td></tr>" >> ${curdir}.pg-html
+		printf "\t\t\t%s\n" "<tr><td><a href="${item}/${item}.pg.html"><b>$item</b></a></td></tr>" >> ${curdir}.pg.html
 	else
-		size=$(du -h $item | cut -f1)
-		printf "\t\t\t%s\n" "<tr><td class="file"><a href="${item}">$item</a></td><td class="size">$size</td></tr>" >> ${curdir}.pg-html
+		printf "\t\t\t%s\n" "<tr><td><a href="${item}">$item</a></td></tr>" >> ${curdir}.pg.html
 	fi
 done
-cat templt >> ${curdir}.pg-html
+printf '		</table>
+	</body>
+</html>' >> ${curdir}.pg.html
 
 for item in $dirs; do
 	itemname=$(basename $item)
 	pardir=$(dirname $item)
 	pardirname=$(basename $pardir)
-	ln -f style.pg-css ${item}/style.pg-css
-	cat templh > ${item}/${itemname}.pg-html
+	printf '<!DOCTYPE html>
+<html>
+	<body>
+		<table>
+' > ${item}/${itemname}.pg.html
 	
-	printf "\t\t\t%s\n" "<tr><th>/$item</th></tr>" >> ${item}/${itemname}.pg-html
+	printf "\t\t\t%s\n" "<tr><td><b>/$item</b></td></tr>" >> ${item}/${itemname}.pg.html
 	if [[ $pardirname = "." ]]; then
-		printf "\t\t\t%s\n" "<tr><td class="dir"><a href="../${curdir}.pg-html">../</a></td><td class="size">-</td></tr>" >> ${item}/${itemname}.pg-html
+		printf "\t\t\t%s\n" "<tr><td><a href="../${curdir}.pg.html"><b>../</b></a></td></tr>" >> ${item}/${itemname}.pg.html
 	else
-		printf "\t\t\t%s\n" "<tr><td class="dir"><a href="../${pardirname}.pg-html">../</a></td><td class="size">-</td></tr>" >> ${item}/${itemname}.pg-html
+		printf "\t\t\t%s\n" "<tr><td><a href="../${pardirname}.pg.html"><b>../</b></a></td></tr>" >> ${item}/${itemname}.pg.html
 	fi
-
-
-	printf "\t\t\t%s\n" "<tr><td class="dir"><a href="${itemname}/${itemname}.pg-html">$itemname</a></td><td class="size">-</td></tr>" >> ${pardir}/${pardirname}.pg-html
+	printf "\t\t\t%s\n" "<tr><td><a href="${itemname}/${itemname}.pg.html"><b>$itemname</b></a></td></tr>" >> ${pardir}/${pardirname}.pg.html
 done
 
 for item in $files; do
 	itemname=$(basename $item)
 	pardir=$(dirname $item)
 	pardirname=$(basename $pardir)
-	size=$(du -h $item | cut -f1)
-	printf "\t\t\t%s\n" "<tr><td class="file"><a href="$itemname">$itemname</a></td><td class="size">$size</td></tr>" >> ${pardir}/${pardirname}.pg-html
+	printf "\t\t\t%s\n" "<tr><td><a href="$itemname">$itemname</a></td></tr>" >> ${pardir}/${pardirname}.pg.html
 done
 
 for item in $dirs; do
 	itemname=$(basename $item)
-	cat templt >> ${item}/${itemname}.pg-html
+	printf '		</table>
+	</body>
+</html>' >> ${item}/${itemname}.pg.html
 done
